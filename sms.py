4afcc6c9bfbf37d18025a4aeac7dfc9cf7a8240d
Rylan <rychao@gmail.com>
2022-06-02 18:31:02 -0700

    working sms

--- sms.py
+++ sms.py
@@ -1,15 +1,17 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
 import os
-import json
 from twilio.rest import Client
 
-account_sid = os.environ['REDACTED']
-auth_token = os.environ['REACTED']
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Hi there',
-                              from_='+REDACTED',
-                              to='1REDACTED'
+                              body='message',
+                              from_='REDACTED',
+                              to='REDACTED'
                           )
 
-# print(message.sid)
+print(message.sid)
